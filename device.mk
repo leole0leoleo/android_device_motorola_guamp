0a91b63a47d5022c35eb2480198dd3fe8d6024cd
snjy4 <92902636+snjy4@users.noreply.github.com>
2022-01-23 04:03:34 +0000

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,11 +1,5 @@
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+### DALVIK
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 DEVICE_PATH := device/motorola/guamp
 BOARD_BUILD_PRODUCT_IMAGE := false
