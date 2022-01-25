820f240666382f0b7a0b95d7de7a37d2d512740b
snjy4 <92902636+snjy4@users.noreply.github.com>
2022-01-25 18:59:23 +0000

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,7 +1,6 @@
-ifneq ($(filter guamp, $(TARGET_DEVICE)),)
-
 LOCAL_PATH := $(call my-dir)
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
-
+ifneq ($(filter guamp, $(TARGET_DEVICE)),)
+  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 endif
