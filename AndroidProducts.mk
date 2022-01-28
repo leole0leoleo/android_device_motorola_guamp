95aa8e3666c19941ceaabe4430607a98120926b2
leo <87917944+leole0leoleo@users.noreply.github.com>
2022-01-28 16:46:26 -0300

    update

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,4 +1,13 @@
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/lineage_guamp.mk
-COMMON_LUNCH_CHOICES := lineage_guamp-userdebug \
-lineage_guamp-eng \
-lineage_guamp-user
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_guamp.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_guamp-user \
+    lineage_guamp-userdebug \
+    lineage_guamp-eng
